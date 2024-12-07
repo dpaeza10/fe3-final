2b09f591d4c92bb2c3625f70029591f9294d3020
Daniela Paez <149510924+dpaeza10@users.noreply.github.com>
2024-12-06 22:33:39 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://dpaeza10.github.io/fe3-final',
+  base: '/fe3-final/',
 })
